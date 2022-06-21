f697b65725327e13fb3b584e59be6f7e939b9024
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-06-21 17:15:27 +0300

    Update Pdf Page Rotator.py

--- Pdf Page Rotator.py
+++ Pdf Page Rotator.py
@@ -1,38 +1,43 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 import easygui as eg
 
-input_pdf_path = eg.fileopenbox(title='Choose a Pdf File.', default='*.pdf')
 
-if input_pdf_path is None:
-    exit()
+def main():
+    input_pdf_path = eg.fileopenbox(title='Choose a Pdf File.', default='*.pdf')
 
-rotate_degreeSize = eg.buttonbox(
-    title='Degree rotation size',
-    msg='By How Many degrees would you like to rotate the pages',
-    choices=['90', '180', '270', '360']
-)
-rotate_degreeSize = int(rotate_degreeSize)
+    if input_pdf_path is None:
+        exit()
 
-save_title = 'Save new pdf as...'
-file_type = '*.pdf'
-eg.msgbox(title='Warning', msg='As you are saving, please append the file name with .pdf extension ')
-saved_pdf_path = eg.filesavebox(title=save_title, default=file_type)
+    rotate_degreeSize = eg.buttonbox(
+        title='Degree rotation size',
+        msg='By How Many degrees would you like to rotate the pages',
+        choices=['90', '180', '270', '360']
+    )
+    rotate_degreeSize = int(rotate_degreeSize)
 
-while saved_pdf_path == input_pdf_path:
-    eg.msgbox('Cannot Overwrite Existing Document')
+    save_title = 'Save new pdf as...'
+    file_type = '*.pdf'
+    eg.msgbox(title='Warning', msg='As you are saving, please append the file name with .pdf extension ')
     saved_pdf_path = eg.filesavebox(title=save_title, default=file_type)
 
-if saved_pdf_path is None:
-    exit()
+    while saved_pdf_path == input_pdf_path:
+        eg.msgbox('Cannot Overwrite Existing Document')
+        saved_pdf_path = eg.filesavebox(title=save_title, default=file_type)
 
-input_file = PdfFileReader(input_pdf_path)
-rotated_pdf = PdfFileWriter()
+    if saved_pdf_path is None:
+        exit()
 
-for page in input_file.pages:
-    page = page.rotateClockwise(rotate_degreeSize)
-    rotated_pdf.addPage(page)
+    input_file = PdfFileReader(input_pdf_path)
+    rotated_pdf = PdfFileWriter()
 
-with open(saved_pdf_path, 'wb') as final_pdf:
-    rotated_pdf.write(final_pdf)
+    for page in input_file.pages:
+        page = page.rotateClockwise(rotate_degreeSize)
+        rotated_pdf.addPage(page)
 
-eg.msgbox(msg='All done', title='Result')
+    with open(saved_pdf_path, 'wb') as final_pdf:
+        rotated_pdf.write(final_pdf)
+
+    eg.msgbox(msg='All done', title='Result')
+
+if __name__ == '__main__':
+    main()
