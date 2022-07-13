dcd1caf8e0821a14105612bbe238481e6c9045ff
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-07-13 22:51:46 +0300

    Update Digital Stream.py

--- Digital Stream.py
+++ Digital Stream.py
@@ -2,7 +2,7 @@ import random, shutil, sys, time
 
 MIN_STREAM_LENGTH = 6  # (!) Try changing this to 1 or 50.
 MAX_STREAM_LENGTH = 14  # (!) Try changing this to 100.
-PAUSE = 0.1  # (!) Try changing this to 0.0 or 2.0.
+PAUSE = 0.3  # (!) Try changing this to 0.0 or 2.0.
 STREAM_CHARS = ['0', '1']  # (!) Try changing this to other characters.
 # Density can range from 0.0 to 1.0:
 DENSITY = 0.1  # (!) Try changing this to 0.10 or 0.30.
